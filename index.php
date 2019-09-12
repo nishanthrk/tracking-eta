07aa6ae823776b12470a2c2dada77e8307d2b274
Nishanth Ravi <jokemeblack@gmail.com>
2019-09-12 07:39:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
     <script src="main.js?<?= strtotime('now') ?>"></script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCGbjymNN5vlknRZgGz8PBbKd7Sulf_bBc&callback=initMap"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap"></script>
 </head>
 <body>
 <div class="container-fluid" ng-app="order-location" ng-controller="controller-order-location" style="margin-top:10px">
